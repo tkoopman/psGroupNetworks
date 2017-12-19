64f5e8242c5172e9cf219bbbc4959e4b4e5b1fa2
Tim Koopman <tim@koopmail.net>
2017-12-20 08:21:56 +1000

    Add project files.

---
+++ psGroupNetworks/GroupNetworks.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psGroupNetworks
+{
+    [Cmdlet(VerbsData.Group, "Networks")]
+    public class GroupNetworks : PSCmdlet
+    {
+        [Parameter(Mandatory = true, ValueFromPipeline = true)]
+        public string[] Input { get; set; }
+
+        private List<Subnet> Subnets { get; set; } = new List<Subnet>();
+
+        protected override void ProcessRecord()
+        {
+            foreach (string i in Input)
+            {
+                Subnets.Add(new Subnet(i));
+            }
+        }
+
+        protected override void EndProcessing()
+        {
+            Subnets.Sort();
+            Subnet last = null;
+            foreach (Subnet s in Subnets)
+            {
+                if (last == null)
+                {
+                    WriteObject(s.Input);
+                    last = s;
+                }
+                else
+                {
+                    if (last.Network == s.Network)
+                    {
+                        WriteVerbose($"{last.Input} equals {s.Input}. Removing duplicate from list.");
+                    }
+                    else if (IPNetwork.Overlap(last.Network, s.Network))
+                    {
+                        WriteVerbose($"{last.Input} overlaps {s.Input}. Removing {s.Input} from list.");
+                    }
+                    else
+                    {
+                        WriteObject(s.Input);
+                        last = s;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ psGroupNetworks/Subnet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psGroupNetworks
+{
+    public class Subnet : IComparable<Subnet>
+    {
+        public Subnet(String input)
+        {
+            Input = input;
+            Network = IPNetwork.Parse(input);
+        }
+
+        public string Input { get; private set; }
+        public IPNetwork Network { get; private set; }
+
+        public int CompareTo(Subnet other)
+        {
+            return Network.CompareTo(other.Network);
+        }
+    }
+}
